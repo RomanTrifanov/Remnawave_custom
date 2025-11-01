bd01106e9ce953c7e3ab8c77c61df6a32f474d56
Roman_T <120475330+RomanTrifanov@users.noreply.github.com>
2025-11-01 16:50:01 +0800

    Create update_user_statuses_icons.sh

---
+++ templates /update_user_statuses_icons.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Проверяем наличие запущенного контейнера 'remnawave'
+CONTAINER=$(docker ps --format "{{.Names}}" | grep ^remnawave$ )
+
+# Проверка наличия контейнеров
+if [[ -z "$CONTAINER" ]]; then
+    echo "Контейнер с именем 'remnawave' не найден."
+    exit 1
+fi
+
+# Скачиваем файл user-statuses.ts
+curl -sSL https://raw.githubusercontent.com/RomanTrifanov/Remnawave_custom/refs/heads/main/templates/user-statuses.ts \
+     -o user-statuses.ts || { echo "Ошибка загрузки файла!"; exit 1; }
+
+# Копируем файл в целевой контейнер
+docker cp user-statuses.ts "${CONTAINER}:/opt/app/libs/contract/constants/templates/user-statuses.ts" && rm user-statuses.ts || {
+    echo "Ошибка копирования файла в контейнер!"
+    exit 1
+}
+
+echo "Файл user-statuses.ts успешно заменён внутри контейнера ${CONTAINER}"
