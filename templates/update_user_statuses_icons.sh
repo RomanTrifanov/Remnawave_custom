0eae5c23b4c8672daed9b9e48a3671cd65208fb8
Roman_T <120475330+RomanTrifanov@users.noreply.github.com>
2025-11-02 11:06:06 +0800

    Update update_user_statuses_icons.sh

--- templates/update_user_statuses_icons.sh
+++ templates/update_user_statuses_icons.sh
@@ -9,18 +9,24 @@ if [[ -z "$CONTAINER" ]]; then
     exit 1
 fi
 
-# Скачиваем файл user-statuses.ts
-curl -sSL https://raw.githubusercontent.com/RomanTrifanov/Remnawave_custom/refs/heads/main/templates/user-statuses.ts \
-     -o user-statuses.ts || { echo "Ошибка загрузки файла!"; exit 1; }
+# Скачиваем файл user-statuses.mod
 
-# Копируем файл в целевой контейнер
-#docker cp user-statuses.ts "${CONTAINER}:/opt/app/libs/contract/constants/templates/user-statuses.ts" || {
-#    echo "Ошибка копирования файла в контейнер!"
-#    exit 1
-#}
+cd /opt/remnawave/
 
-# - ./user-statuses.ts:/opt/app/libs/contract/constants/templates/user-statuses.ts
+curl -sSL https://raw.githubusercontent.com/RomanTrifanov/Remnawave_custom/refs/heads/main/templates/user-statuses.mod \
+     -o user-statuses.mod || { echo "Ошибка загрузки файла!"; exit 1; }
 
-# && rm user-statuses.ts 
+# Копируем оригинальный файл из контейнер (резервная копия)
+docker cp "${CONTAINER}:/opt/app/dist/libs/contract/constants/templates/user-statuses.js" ./user-statuses.js.origin_$(date +%Y-%m-%d_%H-%M-%S)
 
-echo "Файл user-statuses.ts успешно заменён внутри контейнера ${CONTAINER}"
+# Копируем модифицированый файл в целевой контейнер
+docker cp user-statuses.mod "${CONTAINER}:/opt/app/dist/libs/contract/constants/templates/user-statuses.js" || {
+    echo "Ошибка копирования файла в контейнер!"
+    exit 1
+}
+
+echo "Файл user-statuses.js успешно заменён внутри контейнера ${CONTAINER}, перезапускаю контейнер"
+
+# Перезапускаем контейнер для применения изменения
+
+docker restart ${CONTAINER}
